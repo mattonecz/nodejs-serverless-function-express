e884162810c18f13bd06722b15554918da152e08
Matyas Jircik <mattone@seznam.cz>
2024-11-28 00:02:57 +0100

    rename and allow localhost

---
+++ api/currencies.ts
@@ -0,0 +1,15 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const response = await fetch(
+    "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
+  );
+  const data = await response.text();
+  res.setHeader("Access-Control-Allow-Origin", [
+    "https://momence.vercel.app",
+    "http://localhost:3000",
+  ]);
+  return res.json({
+    data,
+  });
+}
