5f13ba56c91f9dd9b6890616e167e95276bfb690
Matyas Jircik <mattone@seznam.cz>
2024-11-30 22:33:39 +0100

    format data

--- api/currencies.ts
+++ api/currencies.ts
@@ -5,8 +5,20 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
   );
   const data = await response.text();
+  const currencies = data
+    .trim()
+    .split("\n")
+    .slice(2)
+    .map((line) => {
+      const [country, currency, amount, code, rate] = line.split("|");
+      return {
+        country: country,
+        currency: currency,
+        amount: parseFloat(amount),
+        code: code,
+        rate: parseFloat(rate),
+      };
+    });
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  return res.json({
-    data,
-  });
+  return res.json(currencies);
 }
